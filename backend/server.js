a7d5ea129ed41040fd32249646f7059144f2b937
Vincent <vince.derrien@gmail.com>
2024-06-17 17:26:44 +0000

    Ajout des routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Hello world!'));
 
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// https://www.youtube.com/watch?v=R4AhvYORZRY&t=2240s
